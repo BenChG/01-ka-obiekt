d001d75fc993c71b97d58e5a93b44750c060561d
Benedykt Chalubinski-Gonerko <ben.chalubinski@gmail.com>
2020-03-10 06:41:30 +0100

    Added feature saving recipient in oneline

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
-#include <iostream>
+#include "MetodyPomocnicze.h"
+#include "MenedzerOsob.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    MetodyPomocnicze metodyPomocnicze;
+    MenedzerOsob menedzerOsob;
+
+    char wybor;
+    while (true)
+    {
+        wybor = metodyPomocnicze.wybierzOpcjeZMenuGlownego();
+
+        switch (wybor)
+        {
+        case '1':
+            menedzerOsob.dodajOsobe();
+            break;
+        case '2':
+            menedzerOsob.wyszukajPoImieniu();
+            break;
+        case '3':
+            menedzerOsob.wyszukajPoNazwisku();
+            break;
+        case '4':
+            menedzerOsob.wyswietlWszystkieOsoby();
+            break;
+        case '9':
+            exit(0);
+            break;
+        default:
+            cout << endl << "Nie ma takiej opcji w menu." << endl << endl;
+            system("pause");
+            break;
+        }
+    }
     return 0;
 }
